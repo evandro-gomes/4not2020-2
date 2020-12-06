79f0e2f0826eae344cabdfa1a641b44bc5da7486
evandro-gomes <evandro.gomes.tec@gmail.com>
2020-12-06 01:34:54 +0000

    12/11 Término do primeiro componente do formulário

--- front-end/src/app/curso/curso-form/curso-form.component.ts
+++ front-end/src/app/curso/curso-form/curso-form.component.ts
@@ -1,4 +1,9 @@
+import { Location } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CursoService } from './../curso.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-curso-form',
@@ -12,15 +17,69 @@ export class CursoFormComponent implements OnInit {
 
   niveis: any = [
     { valor: 'Básico' },
-    { valor: 'Intermediáro' },
+    { valor: 'Intermediário' },
     { valor: 'Avançado' }
   ]
 
   title: string = 'Novo curso'
 
-  constructor() { }
+  constructor(
+    private cursoSrv: CursoService,
+    private snackBar: MatSnackBar,
+    private location: Location,
+    private actRoute: ActivatedRoute
+  ) { }
 
-  ngOnInit(): void {
+  async ngOnInit(){
+    // Verifica se existe o parâmetro id na URL
+    if(this.actRoute.snapshot.params['id']){
+      try{
+        // 1. Acionar o back-end para buscar registro e disponibilizá-lo para edição
+        this.curso = await this.cursoSrv.obterUm(this.actRoute.snapshot.params['id'])
+        // 2. Mudar o título da página
+        this.title = 'Editando curso'
+      }
+      catch(erro){
+        console.log(erro)
+        this.snackBar.open('ERRO: falha ao carregar dados para edição', 'Entendi',
+          {duration: 5000})
+      }
+    }
+  }
+
+  async salvar(form: NgForm){
+    if(form.valid){
+      try{
+        // 1. Salvar os dados no back-end
+        // Se o curso já existir (caso de edição),  ele já terá o atributo _id
+        if(this.curso._id){
+          await this.cursoSrv.atualizar(this.curso) // atualização
+        }
+        else {
+          await this.cursoSrv.novo(this.curso)
+        }
+          // 2. Dar o feddback para o usuario
+        this.snackBar.open('Dados salvos com sucesso.', 'Entendi',
+          {duration: 5000})
+        // 3. Voltar ao componente de listagem 
+        this.location.back()
+      }
+      catch(erro){
+        console.log(erro)
+        this.snackBar.open('ERRO: não foi possível salvar os dados.', 'Entendi',
+          {duration: 5000})
+      }
+    }
+  }
+
+  voltar(form: NgForm){
+    let result = true
+    // form.dirty = formulário sujo, não salvo (via código)
+    // form.touched = o conteúdo de algum campo foi alterado (via usuário)
+    if(form.dirty && form.touched){
+      result = confirm('Dados não salvos. Deseja realmente voltar?')
+    }
+    if(result) this.location.back()
   }
 
 }
